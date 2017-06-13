a5225ff6e6001d33f2a4007c6dcb4ff468677b1c
linfan <dear_moo@qq.com>
2017-06-13 22:06:15 +0800

    增加 选择项目页面

--- src/js/app.js
+++ src/js/app.js
@@ -1,39 +1,43 @@
-var app  = new Framework7({ // 应用初始化
-      activeState: false,
-      init: false,
-      pushState: true
-    }),
-    view = app.addView('.view-main', { // 视图初始化
-      dynamicNavbar: true
-    });
+$(function() {
+  var app  = new Framework7({ // 应用初始化
+        activeState: false,
+        init: false,
+        pushState: true
+      }),
+      view = app.addView('.view-main', { // 视图初始化
+        dynamicNavbar: true
+      });
 
-/* ========== pages ========== */
+  /* ========== pages ========== */
 
-app.onPageInit('*', function (page) {
-  console.log(page.name + ' initialized');
+  app.onPageInit('*', function (page) {
+    console.log(page.name + ' init');
 
-  if (page.name === 'index') {
-    view.showToolbar();
-    $('.navbar').removeClass('navbar-reverse');
-  } else {
-    view.hideToolbar();
-    $('.navbar').addClass('navbar-reverse');
-  }
-});
-
-/* index */
-app.onPageInit('index', function(page) {
-  // handle
-  $('.j-inner').on('click', function() {
-    view.router.loadPage('switch-project.html');
+    if (page.name === 'index') {
+      view.showToolbar();
+      $.hideSidebar();
+      $('.navbar').removeClass('navbar-reverse');
+    } else {
+      view.hideToolbar();
+      $.showSidebar();
+      $('.navbar').addClass('navbar-reverse');
+    }
   });
-});
-/* /index */
 
-/* switch-project */
-app.onPageInit('switch-project', function(page) {
-  // handle
-});
-/* /switch-project */
+  /* index */
+  app.onPageInit('index', function(page) {
+    // handle
+    $('.j-inner').on('click', function() {
+      view.router.loadPage('switch-project.html');
+    });
+  });
+  /* /index */
 
-app.init();
+  /* switch-project */
+  app.onPageInit('switch-project', function(page) {
+    // handle
+  });
+  /* /switch-project */
+
+  app.init();
+})
