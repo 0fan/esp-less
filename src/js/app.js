7f2adb6033ae31dd684793e73de7686a725cb05f
yj <847449213@qq.com>
2017-06-13 16:42:09 +0800

    card

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,4 @@
 $(function() {
-  console.log('成功进入');
   var app=new Framework7({
     init: false,
     cache: false, // 页面缓存
@@ -20,11 +19,40 @@ $(function() {
 
   /* page index */
   app.onPageInit('index', function(page) {
-
     $('#btn-guide').on('click', function() {
       mainView.router.loadPage('switch-project.html?' + Math.random());
     });
-
   });
-/* /page index */
+  /* /page index */
+
+  /* /page switch-project*/
+  app.onPageInit('switch-project', function(page) {
+    var switch_swiper = new Swiper('.switch_swiper', {
+      scrollbar: '.swiper-scrollbar',
+      direction: 'vertical',
+      slidesPerView: 'auto',
+      mousewheelControl: true,
+      freeMode: true,
+      roundLengths : true,
+    });
+    $('.switch_swiper .swiper-box').each(function () {
+      $('.switch_swiper .swiper-box:nth-child(4n + 4)').css({'margin-right':0});
+    })
+    $('.switch_swiper .swiper-box').on('click',function (e) {
+      e.preventDefault();
+      mainView.router.loadPage('identify-step1.html?' + Math.random());
+    })
+    $('.navbar').addClass('navbar-brand no-border');
+    $('.steps .steps-item:eq(0)').addClass('active')
+      .siblings().removeClass('active');
+  });
+  /* /page switch-project */
+
+
+
+
+
+
+
+  app.init();
 })
