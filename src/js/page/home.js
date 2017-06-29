b29801ef2cde76dfdd2435f162180f08c3f694a0
linfan <dear_moo@qq.com>
2017-06-29 18:10:51 +0800

    修改选择项目页面

--- src/js/page/home.js
+++ src/js/page/home.js
@@ -1,4 +1,4 @@
-import Toast from '../widget/toast'
+import toast from '../widget/toast'
 import modal from '../widget/modal'
 import timer from '../widget/timer'
 import steps from '../widget/steps'
@@ -13,11 +13,9 @@ let isOnline = false, // 是否联网
 window.app = new Framework7({ // 应用初始化
   activeState: false,
   init: false,
-  cache: false,
   swipeBackPage: false,
-  preloadPreviousPage: false,
   pushState: true,
-  pushStateNoAnimation: true
+  pushStateNoAnimation: true,
 })
 
 window.view = app.addView('.view-main', { // 视图初始化
@@ -25,10 +23,12 @@ window.view = app.addView('.view-main', { // 视图初始化
 })
 
 $(document).on('pageInit', '.page[data-page=index]', () => {
-  var open = {'identifier': config.termid};
+
+  var open = {'identifier': config.termid}
+
   $.ajax({
     url: url.test + request.allRequest,
-    type: 'GET',
+    type: 'POST',
     dataType: 'json',
     data: {
       merchantId: new Date().getTime(),
@@ -37,31 +37,31 @@ $(document).on('pageInit', '.page[data-page=index]', () => {
     },
   })
     .done(function (d) {
-      if(d.code==0){
-        console.log(d);
-        isInit=true;
-        Toast({text:'服务器链接成功'});
-        $('.toolbar .name > span').text(d.object.propertiesForSale.address);
-        $('.toolbar .tel > span').text(d.object.propertiesForSale.picPhone);
-        $('.j-inner').removeClass('disabled').removeAttr('disabled');
-        store.set('propertiesForSale',d.object.propertiesForSale);
-        open.buildingId = d.object.propertiesForSale.id;
-        canOpen(open);
+
+      if (d.code === 0){
+        console.log(d)
+        isInit=true
+        $('.toolbar .name > span').text(d.object.propertiesForSale.address)
+        $('.toolbar .tel > span').text(d.object.propertiesForSale.picPhone)
+        $('.j-inner').removeClass('disabled').removeAttr('disabled')
+        store.set('propertiesForSale',d.object.propertiesForSale)
+        open.buildingId = d.object.propertiesForSale.id
+        canOpen(open)
       }
     })
     .fail(function (d) {
-      Toast({text:d.message});
+      toast({text:d.message})
     })
     .always(function () {
-      console.log("complete");
       if (!isInit) {
         modal({
+          legend: 'anime-upgrade',
+          status: 'primary',
           title: '正在初始化...',
-          mask: true,
-          full: true
-        });
+          mask: true
+        })
       }
-    });
+    })
 
 
 })
@@ -79,19 +79,9 @@ app.onPageInit('*', function (page) {
   }
 })
 
-
-
-// canvas转化图片
-function convertCanvasToImage(canvas) {
-  var image = new Image();
-  image.src = canvas.toDataURL("image/png");
-  return image;
-}
-
-
 function canOpen(data) {
-  data.redirectUrl = request.canOpen;
-  data.merchantId = new Date().getTime();
+  data.redirectUrl = request.canOpen
+  data.merchantId = new Date().getTime()
   $.ajax({
     url: url.test + request.allRequest,
     type: 'POST',
@@ -100,15 +90,14 @@ function canOpen(data) {
   })
     .done(function (d) {
       if (d.code == 0) {
-        console.log('可继续开户');
+        console.log('可继续开户')
       } else {
-        Toast("不可开户");
+        toast("不可开户")
       }
     })
     .fail(function (d) {
-      Toast("不可开户");
+      toast("不可开户")
     })
 }
-
 
 app.init()
--- src/js/page/step-1.js
+++ src/js/page/step-1.js
@@ -1,21 +1,20 @@
-import {steps1} from '../data/data-steps'
 import steps from '../widget/steps'
+import toast from '../widget/toast'
+import modal from '../widget/modal'
+import timer from '../widget/timer'
 
-import Toast from '../widget/toast'
-import modal from  '../widget/modal'
+import {steps1} from '../data/data-steps'
 
 import config from '../data/data-config'
 import url from '../data/data-url'
 import request from '../data/data-connect'
 
 $(document).on('pageInit', '.page[data-page=switch-project]', () => {
-
+  
   steps({
     data: steps1,
     active: 0
   });
-
-  let propertiesForSaleType;
 
   $.ajax({
     url: url.test + request.allRequest,
@@ -25,82 +24,143 @@ $(document).on('pageInit', '.page[data-page=switch-project]', () => {
       merchantId: new Date().getTime(),
       redirectUrl: request.getBuild,
       termid: config.termid
-    },
+    }
   })
     .done(function (d) {
-      if (d.code == 0) {
+      if (d.code === 0) { // 成功
 
         let _data = d.object.propertiesForSaleType
+        
+        if (!_data.length) { // 无户型数据
+          let t, m
 
-        if (_data.length) {
+          m = modal({
+            legend: 'legend3',
+            status: 'error',
+            title: '当前户型为空',
+            mask: true,
+            action: [
+              {
+                text: '返回首页',
+                onClick: () => {
+                  t.destory()
+                  view.router.loadPage('index.html')
+                }
+              }
+            ]
+          })
+            .on('open', () => {
+              t = timer({
+                time: 10
+              })
+                .on('close', () => {
+                  m.destory()
+                  view.router.loadPage('index.html')
+                })
+            })
+            .on('close', () => {
+              t.destory()
+            })
 
-          let _item = '',
-              _type = 'gaoceng',
-              _name
+          return
+        }
 
-          for (let i = 0, len = _data.length; i < len; i++) {
-            
-            // 获取户型类型，默认为高层
-            if (_data[i].name.match(/\u9AD8\u5C42/)) { // 高层
-              _type = 'gaoceng'
-            }
+        let _item = '',
+            _type = 'gaoceng',
+            _name
 
-            if (_data[i].name.match(/\u5927\u5E73\u5C42/)) { // 大平层
-              _type = 'dapingceng'
-            }
-
-            if (_data[i].name.match(/\u6D0B\u623F/)) { // 洋房
-              _type = 'yangfang'
-            }
-
-            if (_data[i].name.match(/\u522B\u5885/)) { // 别墅
-              _type = 'bieshu'
-            }
-
-            if (_data[i].name.match(/\u5546\u4E1A/)) { // 商业
-              _type = 'shangye'
-            }
-
-            if (_data[i].name.match(/\u5546\u94FA/)) { // 商铺
-              _type = 'shangpu'
-            }
-            
-            _item += '<div class="switchProject-items_item ' + _type + '"\
-                        data-equalMoney =' + _data[i].equalMoney + '\
-                        data-iTime      =' + _data[i].iTime + '\
-                        data-id         =' + _data[i].id + '\
-                        data-name       =' + _data[i].name + '\
-                        data-price      =' + _data[i].price + '\
-                        data-type       =' + _data[i].type + '\
-                      >'
-            _name = '<div class=info>' + (_data[i].name || '默认户型') + '</div>'
-            _item += _name
-            _name += '</div>'
-
+        for (let i = 0, len = _data.length; i < len; i++) {
+          
+          // 获取户型类型，默认为高层
+          if (_data[i].name.match(/\u9AD8\u5C42/)) { // 高层
+            _type = 'gaoceng'
           }
 
-          $(_item).appendTo($('.j-switchProject-items .switchProject-items_wrap'))
+          if (_data[i].name.match(/\u5927\u5E73\u5C42/)) { // 大平层
+            _type = 'dapingceng'
+          }
 
-        } else {
-          Toast({text:d.message});
-          view.router.loadPage('index.html');
+          if (_data[i].name.match(/\u6D0B\u623F/)) { // 洋房
+            _type = 'yangfang'
+          }
+
+          if (_data[i].name.match(/\u522B\u5885/)) { // 别墅
+            _type = 'bieshu'
+          }
+
+          if (_data[i].name.match(/\u5546\u4E1A/)) { // 商业
+            _type = 'shangye'
+          }
+
+          if (_data[i].name.match(/\u5546\u94FA/)) { // 商铺
+            _type = 'shangpu'
+          }
+          
+          _item += '<div class="switchProject-items_item ' + _type + '"\
+                      data-equalMoney =' + _data[i].equalMoney + '\
+                      data-iTime      =' + _data[i].iTime + '\
+                      data-id         =' + _data[i].id + '\
+                      data-name       =' + _data[i].name + '\
+                      data-price      =' + _data[i].price + '\
+                      data-type       =' + _data[i].type + '\
+                    >'
+          _name = '<div class=info>' + (_data[i].name || '默认户型') + '</div>'
+          _item += _name
+          _name += '</div>'
+
         }
+
+        $(_item).appendTo($('.j-switchProject-items .switchProject-items_wrap'))
+        
+      }
+      if (d.code === -1) {
+
+        let t, m
+
+        m = modal({
+          legend: 'legend3',
+          status: 'error',
+          title: d.error,
+          info: d.message,
+          full: true,
+          action: [
+            {
+              text: '返回首页',
+              onClick: () => {
+                t.destory()
+                view.router.loadPage('index.html')
+              }
+            }
+          ]
+        })
+          .on('open', () => {
+            t = timer({
+              time: 10
+            })
+              .on('close', () => {
+                m.destory()
+                view.router.loadPage('index.html');
+              })
+          })
+          .on('close', () => {
+            t.destory()
+          }) // 失败
+
       }
     })
     .fail(function (d) {
-      view.router.loadPage('index.html');
-      Toast({text:d.message});
+      toast({
+        text: '服务器连接失败'
+      })
+      view.router.loadPage('index.html')
     })
-    .always(function () {
-      console.log('complete');
-    });
 
     // handle
     $('.switchProject-items_wrap').on('click', '.switchProject-items_item', function (e) {
 
-      store.set('propertiesForSaleType',$(this).data());
+      store.set('propertiesForSaleType', $(this).data())
 
-      view.router.loadPage('identify-step1.html');
+      view.router.loadPage('identify-step1.html')
 
     });
 
