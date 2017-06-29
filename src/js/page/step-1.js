9a974947f3215d400b99a5e88b32dce13bd1f17a
linfan <dear_moo@qq.com>
2017-06-29 16:14:32 +0800

    修改选择项目页面

--- src/js/page/step-1.js
+++ src/js/page/step-1.js
@@ -15,7 +15,7 @@ $(document).on('pageInit', '.page[data-page=switch-project]', () => {
     active: 0
   });
 
-  var propertiesForSaleType;
+  let propertiesForSaleType;
 
   $.ajax({
     url: url.test + request.allRequest,
@@ -29,35 +29,79 @@ $(document).on('pageInit', '.page[data-page=switch-project]', () => {
   })
     .done(function (d) {
       if (d.code == 0) {
-        if (d.object.propertiesForSaleType.length != 0) {
-          console.log(d);
 
-          
-          _.each(d.object.propertiesForSaleType, function (item, index, arr) {
-            $("<div data-propertiesForSaleType='" + (JSON.stringify(item)) + "' class='switchProject-items_item'>\
-                <div class='info'> " + item.name + " </div>\
-              </div>")
-              .appendTo($('.j-switchProject-items .switchProject-items_wrap'));
-          });
+        let _data = d.object.propertiesForSaleType
+
+        if (_data.length) {
+
+          let _item = '',
+              _type = 'gaoceng',
+              _name
+
+          for (let i = 0, len = _data.length; i < len; i++) {
+            
+            // 获取户型类型，默认为高层
+            if (_data[i].name.match(/\u9AD8\u5C42/)) { // 高层
+              _type = 'gaoceng'
+            }
+
+            if (_data[i].name.match(/\u5927\u5E73\u5C42/)) { // 大平层
+              _type = 'dapingceng'
+            }
+
+            if (_data[i].name.match(/\u6D0B\u623F/)) { // 洋房
+              _type = 'yangfang'
+            }
+
+            if (_data[i].name.match(/\u522B\u5885/)) { // 别墅
+              _type = 'bieshu'
+            }
+
+            if (_data[i].name.match(/\u5546\u4E1A/)) { // 商业
+              _type = 'shangye'
+            }
+
+            if (_data[i].name.match(/\u5546\u94FA/)) { // 商铺
+              _type = 'shangpu'
+            }
+            
+            _item += '<div class="switchProject-items_item ' + _type + '"\
+                        data-equalMoney =' + _data[i].equalMoney + '\
+                        data-iTime      =' + _data[i].iTime + '\
+                        data-id         =' + _data[i].id + '\
+                        data-name       =' + _data[i].name + '\
+                        data-price      =' + _data[i].price + '\
+                        data-type       =' + _data[i].type + '\
+                      >'
+            _name = '<div class=info>' + (_data[i].name || '默认户型') + '</div>'
+            _item += _name
+            _name += '</div>'
+
+          }
+
+          $(_item).appendTo($('.j-switchProject-items .switchProject-items_wrap'))
+
         } else {
-          Toast({test:d.message});
+          Toast({text:d.message});
           view.router.loadPage('index.html');
         }
       }
     })
     .fail(function (d) {
       view.router.loadPage('index.html');
-      Toast({test:d.message});
+      Toast({text:d.message});
     })
     .always(function () {
-      console.log("complete");
+      console.log('complete');
     });
 
     // handle
     $('.switchProject-items_wrap').on('click', '.switchProject-items_item', function (e) {
-      propertiesForSaleType = JSON.parse($(this).attr('data-propertiesForSaleType'));
-      store.set('propertiesForSaleType',propertiesForSaleType);
+
+      store.set('propertiesForSaleType',$(this).data());
+
       view.router.loadPage('identify-step1.html');
+
     });
 
 })
