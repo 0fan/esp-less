892ef1e88eae1383ef06c7d835cdced5501aa2e9
linfan <dear_moo@qq.com>
2017-06-29 09:44:30 +0800

    增加升级动画

--- src/js/page/home.js
+++ src/js/page/home.js
@@ -26,13 +26,6 @@ window.view = app.addView('.view-main', { // 视图初始化
 
 $(document).on('pageInit', '.page[data-page=index]', () => {
 
-  if (!isInit) {
-    modal({
-      title: '正在初始化...',
-      mask: true,
-      full: true
-    });
-  }
 
 })
 
