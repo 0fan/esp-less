c0d4f8d60ef1a985719df5b090379fe166662034
yj <847449213@qq.com>
2017-07-12 13:00:47 +0800

    add

--- src/js/data/data-url.js
+++ src/js/data/data-url.js
@@ -1,6 +1,8 @@
 let url = {
   // test: 'http://10.0.10.97:8080/'
-  test: 'http://www.1m2.net/test/'
+  // test: 'http://www.1m2.net/test/'
+  test: 'http://10.0.10.99:8080/'
+
 }
 
 export default url
