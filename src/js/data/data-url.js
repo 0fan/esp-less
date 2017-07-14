817d64a700eccbe5f46fa1e9bc91eae4756250e6
yj <847449213@qq.com>
2017-07-14 16:33:07 +0800

    二维码阴影效果

--- src/js/data/data-url.js
+++ src/js/data/data-url.js
@@ -1,13 +1,7 @@
 let url = {
-<<<<<<< HEAD
-  test: 'http://10.0.10.99:8080/'
-  // test: 'http://www.1m2.net/test/'
-=======
   // test: 'http://10.0.10.97:8080/'
   // test: 'http://www.1m2.net/test/'
   test: 'http://10.0.10.99:8080/'
-
->>>>>>> 83b7ffc45657e81478b6b551555ef74f2a4d11ca
 }
 
 export default url
